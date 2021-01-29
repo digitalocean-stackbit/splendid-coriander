26b681f525d6a2a1b025dc1133ea613a280c7dee
digitalocean-stackbit <77975946+digitalocean-stackbit@users.noreply.github.com>
2021-01-29 12:22:53 +0200

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -9,6 +9,6 @@ echo "stackbit-build.sh: start build"
 # build site
 jekyll build
 
-./inject-stackbit-widget.js _site https://localhost:8092/init.js 601336bcfb1ea3806ad4871e
+# ./inject-stackbit-widget.js _site https://localhost:8092/init.js 601336bcfb1ea3806ad4871e
 
 echo "stackbit-build.sh: finished build"
